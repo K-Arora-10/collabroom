10e0ed86bb746c6b70d8b2a779157e6522e76d1c
Krish <krisharora984@gmail.com>
2025-11-12 02:48:10 +0530

    Refersh Token

--- collabroom-frontend/src/context/AuthContext.jsx
+++ collabroom-frontend/src/context/AuthContext.jsx
@@ -6,21 +6,31 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
- 
+  async function fetchProtected() {
+    return await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/protected`, {
+      credentials: "include",
+    });
+  }
+
+  async function refreshAccessToken() {
+    await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/refresh`, {
+      method: "POST",
+      credentials: "include",
+    });
+  }
+
   useEffect(() => {
     async function checkAuth() {
       try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/protected`, {
-          credentials: "include",
-        });
-        if(!res.ok)
-        {
-          const data = await res.json();
-          console.log(data);
+        let res = await fetchProtected();
+
+        if (res.status === 401) {
+          await refreshAccessToken();
+          res = await fetchProtected();
         }
+
         if (res.ok) {
           const data = await res.json();
-          console.log(data);
           setUser(data);
         } else {
           setUser(null);
